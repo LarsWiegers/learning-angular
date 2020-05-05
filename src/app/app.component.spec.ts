c8d5cfbf1e916baa5238d2fbcec3447df00ba5a6
Lars Wiegers <larswiegers@live.nl>
2020-05-05 21:50:44 +0200

    we nolonger use the default page, so this test does not make sense anymore

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('riskchallenger-test');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('riskchallenger-test app is running!');
-  });
 });
