c59640f42aa7d2d73984078cf0953721ce7c9593
Lars Wiegers <larswiegers@live.nl>
2020-05-07 19:09:18 +0200

    add a green color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
       colors: {
         'primary': 'rgb(63,71,88)',
         'primary-100': 'rgb(160,174,192)',
-        'primary-900': 'rgb(48,68,93)'
+        'primary-900': 'rgb(48,68,93)',
+        'primary-green': 'rgb(34, 107, 69)',
       }
     },
   },
